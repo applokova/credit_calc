0427a10daa963df310f4e688b3a188fe5e8d4b8f
applokova <stasya133@mail.ru>
2022-07-27 22:09:58 +0300

    v1

---
+++ CreditPaymentService.java
@@ -0,0 +1,15 @@
+public class CreditPaymentService {
+    public int calculate(double sum, double bid, int per) {
+        int result;
+        if (per >= 1) {
+            int months = (per * 12);
+            double xBids = bid / 100 / 12;
+            double pow = Math.pow(1 + xBids,months);
+            double edd = (sum * ( xBids + (xBids / ( pow - 1 ) ) ));
+            result = (int) edd;
+        } else {
+            result = 0;
+        }
+        return result;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int a = service.calculate(1000000, 9.99, 1);
+        int b = service.calculate(1000000, 9.99, 2);
+        int c = service.calculate(1000000, 9.99, 3);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
