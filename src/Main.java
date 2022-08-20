ad73957742614cda3bbffecf6e6a5e28ef23bd99
applokova <stasya133@mail.ru>
2022-08-20 23:30:37 +0300

    доработка2

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import src.CreditPaymentService;
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int a = service.calculate(1000000, 9.99, 1);
-        int b = service.calculate(1000000, 9.99, 2);
-        int c = service.calculate(1000000, 9.99, 3);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        int resulOne = service.calculate(1000000, 9.99, 1);
+        int resultTwo = service.calculate(1000000, 9.99, 2);
+        int resultThree = service.calculate(1000000, 9.99, 3);
+        System.out.println(resulOne);
+        System.out.println(resultTwo);
+        System.out.println(resultThree);
     }
 }
